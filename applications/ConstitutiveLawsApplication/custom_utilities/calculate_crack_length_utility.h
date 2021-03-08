c599a4bd0be583cac17b607c9f4277a8d144f18c
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2021-03-08 13:58:46 +0100

    adding process

--- applications/ConstitutiveLawsApplication/custom_utilities/calculate_crack_length_utility.h
+++ applications/ConstitutiveLawsApplication/custom_utilities/calculate_crack_length_utility.h
@@ -10,3 +10,188 @@
 //  Main authors:   Alejandro Cornejo
 //  Collaborator:
 //
+
+#if !defined(KRATOS_CALCULATE_CRACK_LENGTH_UTILITY_H_INCLUDED)
+#define KRATOS_CALCULATE_CRACK_LENGTH_UTILITY_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+
+#include "structural_mechanics_application_variables.h"
+
+namespace Kratos
+{
+
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+/**
+ * @class CalculateCrackLengthUtility
+ * @ingroup ConstitutiveLawsApplication
+ * @brief This utility computed the crack length in damage mechanics
+ * @authors Alejandro Cornejo
+ */
+class CalculateCrackLengthUtility
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    /// Pointer definition of CalculateCrackLengthUtility
+    KRATOS_CLASS_POINTER_DEFINITION(CalculateCrackLengthUtility);
+
+    /// Definition of size type
+    typedef std::size_t SizeType;
+
+    /// Definition of index type
+    typedef std::size_t IndexType;
+
+    /// Definition of the zero tolerance
+    static constexpr double tolerance = std::numeric_limits<double>::epsilon();
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor
+    CalculateCrackLengthUtility()
+    {
+    }
+
+    /// Destructor.
+    virtual ~CalculateCrackLengthUtility() {}
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    /// Assignment operator.
+    CalculateCrackLengthUtility &operator=(CalculateCrackLengthUtility const &rOther);
+};
+} // namespace Kratos.
+#endif // KRATOS_CALCULATE_CRACK_LENGTH_UTILITY_H_INCLUDED  defined
